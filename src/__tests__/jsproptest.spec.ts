e650f76419d98a9cb599aaf8eb535b95f899b3e9
Won-Wook Hong <jradoo@gmail.com>
2021-01-02 11:10:16 +0900

    introduce jsproptest v0.1.1

---
+++ src/__tests__/jsproptest.spec.ts
@@ -0,0 +1,11 @@
+import { forAll, Property, interval, stringGen } from 'jsproptest'
+
+describe('jsproptest', () => {
+    it('basic', () => {
+        const prop = new Property((a:number, b:string) => {
+            return a < 10 || b.length > 3
+        })
+
+        prop.forAll(interval(0,10), stringGen(0, 10))
+    })
+})
